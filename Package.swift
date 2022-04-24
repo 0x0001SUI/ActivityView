20f83756b53ce800840edf4edb65590147d1fda4
Dima Koskin <dmkskn@icloud.com>
2022-04-24 15:33:12 +0300

    Change the interface to Environment

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ActivityView",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
         .library(
--- Sources/ActivityView/ActivityView.swift
+++ Sources/ActivityView/ActivityView.swift
@@ -2,6 +2,39 @@
 import SwiftUI
 
 
+internal extension UIWindow {
+    @available(iOS 13, *)
+    static var topViewController: UIViewController? {
+        guard var topController = keyWindow?.rootViewController else {
+            return nil
+        }
+        
+        while let presentedViewController = topController.presentedViewController {
+            topController = presentedViewController
+        }
+        
+        return topController
+    }
+    
+    @available(iOS 13, *)
+    static var keyWindow: UIWindow? {
+        if #available(iOS 15, *) {
+            guard let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+                return nil
+            }
+
+            return scene.keyWindow
+        } else {
+            guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else {
+                return nil
+            }
+            
+            return window
+        }
+    }
+}
+
+
 @available(iOS 13, *)
 internal struct ActivityView: ViewModifier {
     @Binding var isPresented: Bool
@@ -34,13 +67,32 @@ internal struct ActivityView: ViewModifier {
 
 extension View {
     /// A popover view that you use to offer standard services from your app.
-    @available(iOS 13, *)
+    @available(swift, deprecated: 1.0.0, obsoleted: 2.0.0, message: "Use @Environment(\\.activityView) instead.")
     public func activityView(
         isPresented: Binding<Bool>,
         for items: [Any],
-        without excludedTypes: [UIActivity.ActivityType]? = nil)
+        without excludedTypes: [UIActivity.ActivityType]? = nil
+    )
     -> some View {
         modifier(ActivityView(isPresented: isPresented, items: items, excludedTypes: excludedTypes))
     }
 }
+
+
+private struct ActivityViewKey: EnvironmentKey {
+    static let defaultValue: (_ items: [Any], _ excludedTypes: [UIActivity.ActivityType]?) -> Void = { items , excludedTypes in
+        let controller = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        controller.excludedActivityTypes = excludedTypes
+        UIWindow.topViewController?.present(controller, animated: true, completion: nil)
+    }
+}
+
+
+extension EnvironmentValues {
+    public var activityView: (_ items: [Any], _ excludedTypes: [UIActivity.ActivityType]?) -> Void {
+        get { self[ActivityViewKey.self] }
+        set { self[ActivityViewKey.self] = newValue }
+    }
+}
+
 #endif
